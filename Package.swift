7d4e267d782e002806c610d8b956ba349355478f
Achraf Ameddah <achraf.ameddah@egym.com>
2022-11-04 10:34:40 +0100

    Add spm support for capacitor-plugin

---
+++ Package.swift
@@ -0,0 +1,49 @@
+// swift-tools-version: 5.6
+import PackageDescription
+
+let package = Package(
+    name: "HTTP Plugin Package",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "HTTPPlugin",
+            targets: ["HTTPPlugin"]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/ionic-team/capacitor-swift-pm",
+            .upToNextMajor(from: "3.7.0")
+        ),
+    ],
+    targets: [
+        .target(
+            name: "CHTTPPlugin",
+            dependencies: [
+                .product(
+                    name: "Capacitor",
+                    package: "capacitor-swift-pm"
+                ),
+            ],
+            path: "Sources/CHTTPPlugin"
+        ),
+        .target(
+            name: "HTTPPlugin",
+            dependencies:
+            [
+                .product(
+                    name: "Capacitor",
+                    package: "capacitor-swift-pm"
+                ),
+                .product(
+                    name: "Cordova",
+                    package: "capacitor-swift-pm"
+                ),
+                "CHTTPPlugin",
+            ],
+            path: "Sources/HTTPPlugin"
+        ),
+    ]
+)
